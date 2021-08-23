566445eaf1fdcc2968e359e71d88c487cd6c1180
3DiQNaveen <80444474+3DiQNaveen@users.noreply.github.com>
2021-08-23 19:54:39 +0530

    initial commit

---
+++ PushCode/src/pushcode/Push.java
@@ -0,0 +1,18 @@
+package pushcode;
+
+public class Push {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World ");
+		
+		System.out.println("Test Automation");
+		
+		System.out.println("Github code push");
+		
+		System.out.println("jenkins exploring");
+
+	}
+
+}
